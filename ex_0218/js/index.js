0c4034fdee00eeeaec6efd3bdd88bab199ced1de
Jeon-Hyoung-woon <guddns30@gmail.com>
2025-02-18 17:25:02 +0900

    Update index.js

--- ex_0218/js/index.js
+++ ex_0218/js/index.js
@@ -24,7 +24,7 @@ d.getElementById("demo4").innerHTML =
 
 const x1 = d.querySelectorAll("p.intro3");
 d.getElementById("demo5").innerHTML =
-  "클래스 intro3 중 두번째 문단 : " + x1[1].innerHTML;
+  "셀렉터를 사용한 클래스 intro3 중 두번째 문단 : " + x1[1].innerHTML;
 
 const x2 = d.forms["frm1"];
 let text = "";
