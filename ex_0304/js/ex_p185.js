8496720c5154eea8cfa2c16b3040e8b67bcae33c
Jeon-Hyoung-woon <guddns30@gmail.com>
2025-03-04 10:41:47 +0900

    Book

---
+++ ex_0304/js/ex_p185.js
@@ -0,0 +1,10 @@
+import {fncA, fncB, msg} from "./module_pattern_1_c_test.js";
+import {fncC, fncD} from "./module_pattern_2_c_test.js";
+import fncE from "./module_pattern_3_c_test.js";
+
+fncA();
+fncB();
+console.log(msg);
+fncC();
+fncD();
+fncE();
